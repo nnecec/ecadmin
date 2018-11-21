e8553518db19a72bec9018270c7416fa05b89c84
nnecec <nnecec@outlook.com>
2018-11-21 20:07:24 +0800

    update theme

--- src/app/account/account.component.spec.ts
+++ src/app/account/account.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { AccountComponent } from './account.component';
+import { AccountComponent } from './account.component'
 
 describe('AccountComponent', () => {
-  let component: AccountComponent;
-  let fixture: ComponentFixture<AccountComponent>;
+  let component: AccountComponent
+  let fixture: ComponentFixture<AccountComponent>
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AccountComponent ]
     })
-    .compileComponents();
-  }));
+    .compileComponents()
+  }))
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AccountComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(AccountComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
